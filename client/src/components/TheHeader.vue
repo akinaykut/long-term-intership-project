95271533246e9f0616f908935407f16a796c6e98
akinaykut05 <akinaykut05@gmail.com>
2022-10-07 11:29:57 +0300

    Created TheHeader component

---
+++ client/src/components/TheHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav>
+    <div class="container">
+      <div class="navbar-header container">
+        <a class="navbar-brand" href="/"
+          ><img src="" class="img-responsive margin-top10" alt="Cimer Logo"
+        /></a>
+        <a
+          class="navbar-brand pull-right"
+          href="http://www.iletisim.gov.tr"
+          target="_blank"
+          ><img
+            src="/images/iletisim-baskanligi-logo.png?v=_id-guvBHUWtVQ43foldRqavdgQvBumAvLLVFVq8sZU"
+            class="img-responsive"
+            alt="İletişim Başkanlığı Logo"
+        /></a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  height: 2%;
+  padding: 2%;
+  background-color: rgb(88, 85, 85);
+}
+.navbar-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
