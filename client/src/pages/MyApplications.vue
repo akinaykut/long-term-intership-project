357cb0013c0c12e622eab3f4bccf538ef4060a3f
akinaykut05 <akinaykut05@gmail.com>
2022-10-12 13:16:30 +0300

    Created application pages

---
+++ client/src/pages/MyApplications.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>My Applications</h1>
+</template>
--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -2,17 +2,28 @@ import { createRouter, createWebHistory } from "vue-router";
 import AdminLogin from "../pages/AdminLogin.vue";
 import AdminDashboard from "../pages/AdminDashboard.vue";
 import TheMainPage from "../pages/TheMainPage.vue";
+import TheApplication from "../pages/TheApplication.vue";
+import MyApplications from "../pages/MyApplications.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: "/admin-login",
-      component: AdminLogin,
-    },
-    {
       path: "/",
       component: TheMainPage,
     },
+    {
+      path: "/application",
+      component: TheApplication,
+    },
+    {
+      path: "/application/my-applications",
+      component: MyApplications,
+    },
+    {
+      path: "/admin-login",
+      component: AdminLogin,
+    },
+
     {
       path: "/admin-dashboard",
       component: AdminDashboard,
