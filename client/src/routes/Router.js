2f622062885e5601b2cc25db06fea9f0ab42b35b
akinaykut05 <akinaykut05@gmail.com>
2022-10-12 13:00:20 +0300

    Updated admin login page and admin dashboard

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -1,13 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AdminLogin from "../pages/AdminLogin.vue";
+import AdminDashboard from "../pages/AdminDashboard.vue";
 import TheMainPage from "../pages/TheMainPage.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/admin-login", component: AdminLogin },
+    {
+      path: "/admin-login",
+      component: AdminLogin,
+    },
     {
       path: "/",
       component: TheMainPage,
+    },
+    {
+      path: "/admin-dashboard",
+      component: AdminDashboard,
     },
   ],
 });
