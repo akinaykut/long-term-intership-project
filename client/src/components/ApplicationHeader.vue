888cc71ead401fa46e5dfb0d177fa086530721d3
akinaykut05 <akinaykut05@gmail.com>
2022-10-12 15:04:57 +0300

    Added application header

---
+++ client/src/components/ApplicationHeader.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 application-header-left">
+        <ul>
+          <li class="active">
+            <span class="step">1</span>
+            <span class="steptext">Kişisel Bilgiler</span>
+          </li>
+          <li class="">
+            <span class="step">2</span>
+            <span class="steptext">Başvuru Detayı</span>
+          </li>
+          <li class="">
+            <span class="step">3</span>
+            <span class="steptext">Başvuruyu Tamamla</span>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-4 application-header-right">
+        <button class="btn btn-primary">Başvurularım</button>
+        <button class="btn btn-danger">Çıkış Yap</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+ul {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  list-style-type: none;
+  padding-left: 0;
+}
+
+li {
+  padding: 10px;
+  border-radius: 5px;
+}
+.application-header-left {
+  padding: 0;
+}
+.application-header-right {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 0;
+}
+.step {
+  background: #fff;
+  color: grey;
+  display: inline-block;
+  font-weight: 700;
+  line-height: 1.3em;
+  margin-right: 5px;
+  text-align: center;
+  width: 1.3em;
+  border-radius: 15px;
+}
+
+.active {
+  color: #fff;
+  background-color: rgb(25, 181, 212);
+}
+</style>
--- client/src/pages/TheApplication.vue
+++ client/src/pages/TheApplication.vue
@@ -1,3 +1,31 @@
 <template>
-  <h1>Application</h1>
+  <div>
+    <the-header></the-header>
+    <div class="content container">
+      <application-header></application-header>
+    </div>
+
+    <the-footer></the-footer>
+  </div>
 </template>
+
+<script>
+import ApplicationHeader from "../components/ApplicationHeader.vue";
+import TheFooter from "../components/TheFooter.vue";
+import TheHeader from "../components/TheHeader.vue";
+
+export default {
+  components: {
+    ApplicationHeader,
+    TheHeader,
+    TheFooter,
+  },
+};
+</script>
+
+<style scoped>
+.content {
+  padding-top: 10%;
+  height: 100vh;
+}
+</style>
