1f037b5b01ec2c2a246bcd1f4d7bb7f90386d8f2
akinaykut05 <akinaykut05@gmail.com>
2022-12-21 10:23:36 +0300

    Add frontend start template

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,4 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import Router from "./routes/Router";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
-import TheHeader from "./components/TheHeader.vue";
-import TheFooter from "./components/TheFooter.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const app = createApp(App);
-
-app.use(Router);
-
-app.component("the-header", TheHeader);
-app.component("the-footer", TheFooter);
-
-app.mount("#app");
+createApp(App).mount('#app')
--- client/src/pages/MyApplications.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>My Applications</h1>
-</template>
